b27b89e51daa486569dc11a7314e4645cfb48ca0
danish1658 <danish@amperative.com>
2023-08-22 13:06:57 +0530

    Changed Colors

--- packages/moti/src/skeleton/shared.ts
+++ packages/moti/src/skeleton/shared.ts
@@ -1,7 +1,7 @@
 export const DEFAULT_SKELETON_SIZE = 32
 
 export const baseColors = {
-  dark: { primary: 'rgb(17, 17, 17)', secondary: 'rgb(51, 51, 51)' },
+  dark: { primary: 'rgb(107, 66, 127))', secondary: 'rgb(77, 23, 107)' },
   light: {
     primary: 'rgb(250, 250, 250)',
     secondary: 'rgb(205, 205, 205)',
